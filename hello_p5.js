256fc2dc85eef2900cd43d1b42096c9f12ec834d
polycephalus <juamalin@gmail.com>
2019-04-08 00:41:05 +0200

    ellipse

---
+++ hello_p5.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+  createCanvas(600, 400);
+  background(255, 200, 200);
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(mouseX, mouseY);
+}
